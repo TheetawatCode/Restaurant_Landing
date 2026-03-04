5466dcca45e1a349a05d3272ad4a58fbb5376658
Theetawat Premsawat <theetawat.psw@gmail.com>
2026-03-04 18:19:19 +0700

    commit

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,8 +1,8 @@
 export const GALLERY = [
-    { src: "/images/dish-1.jpg", alt: "Handmade pasta close-up" },
-    { src: "/images/dish-2.jpg", alt: "Pizza with basil" },
-    { src: "/images/dish-3.jpg", alt: "Restaurant interior" },
-    { src: "/images/dish-4.jpg", alt: "Dessert plated beautifully" },
-    { src: "/images/dish-5.jpg", alt: "Wine and appetizers" },
-    { src: "/images/dish-6.jpg", alt: "Chef plating dish" },
+    { src: "/images/signature.jpg", alt: "Handmade pasta close-up" },
+    { src: "/images/menu-pizza.jpg", alt: "Pizza with basil" },
+    { src: "/images/hero.jpg", alt: "Restaurant interior" },
+    { src: "/images/menu-tiramisu.jpg", alt: "Dessert plated beautifully" },
+    { src: "/images/menu-wine.jpg", alt: "Wine and appetizers" },
+    { src: "/images/menu-finedish.jpg", alt: "Chef plating dish" },
   ];
